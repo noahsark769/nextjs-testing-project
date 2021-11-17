0aa5f2b51cff220388e5e04f82a1049537893ab7
Noah Gilmore <noah.w.gilmore@gmail.com>
2021-11-17 13:21:46 -0800

    Initial thing and the out directory

--- src/Heading1.jsx
+++ src/Heading1.jsx
@@ -1,15 +1,10 @@
 import * as React from "react";
-import { Builder } from "@builder.io/react";
 
-export const Heading1 = (props) => <h1>{props.text}</h1>;
-
-Builder.registerComponent(Heading1, {
-    name: "Custom Heading 1",
-    inputs: [
-        {
-            name: "text",
-            type: "string",
-            defaultValue: "hello world",
-        },
-    ],
-});
+export const Heading1 = (props) => {
+    const [word, setWord] = React.useState("A")
+    return <div>
+        <h1>Heading Selected: {word}</h1>
+        <button onClick={() => setWord("A")}>Set A</button>
+        <button onClick={() => setWord("B")}>Set B</button>
+    </div>;
+}
