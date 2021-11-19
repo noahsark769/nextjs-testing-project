a94c4100ef27b2e0b60635ef01a5f0a6df6c7003
Noah Gilmore <noah.w.gilmore@gmail.com>
2021-11-18 16:49:56 -0800

    Put a pic

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,12 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import { Heading1 } from '../src/Heading1'
+import Image from 'next/image'
+import alchemy from '../public/alchemy.png'
+
+const justReturnUrlLoader = ({ src, width, quality }) => {
+  return `http://localhost:3001${src}`
+}
 
 export default function Home() {
   return (
@@ -32,6 +38,15 @@ export default function Home() {
           <h1>Content to push the link down</h1>
           <h1>Content to push the link down</h1>
           <h1>Content to push the link down3 </h1>
+          <div style={{ width: "300px", height: "300px", position: "relative" }}>
+            <Image
+              loader={justReturnUrlLoader}
+              src={alchemy}
+              alt="Alchemy"
+              layout="fill"
+              objectFit="contain"
+            />
+          </div>
           <h1>Content to push the link down</h1>
           <h1>Content to push the link down</h1>
           <h1>Content to push the link down</h1>
